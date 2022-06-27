1e98139766e9877454ca8c2892be71849da76820
hung-cell <daoquanghung922@gmail.com>
2022-06-27 22:03:06 +0700

    fix bug 3

--- order-service/src/main/java/com/assignment/n15/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/assignment/n15/orderservice/controller/OrderController.java
@@ -26,6 +26,17 @@ public class OrderController {
         return orderservice.getOrderByCode(code);
     }
 
-
+    @GetMapping("/get_order_detail/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<OrderDetailResponse> getOrderDetailsByOrderNumber(@PathVariable Long id){
+//        Optional<OrderDetailsListDto> orderDetailByOrderNumber =
+//                orderDetailServiceClient.getOrderDetailByOrderNumber(id);
+//        if(orderDetailByOrderNumber.isPresent()){
+//            return orderDetailByOrderNumber.get().getOrderDetailResponses();
+//        }else{
+//            return null;
+//        }
+        return orderDetailServiceClient.getOrderDetailByOrderNumber(id);
+    }
 
 }
